114f7fbe33147129e548bdff1e0923c34ff50f82
Kyle Harrington <kharrington@uidaho.edu>
2016-07-08 17:47:47 +0200

    Adding license

--- src/java/sc/fiji/ThreeDViewerLauncher.java
+++ src/java/sc/fiji/ThreeDViewerLauncher.java
@@ -1,10 +1,12 @@
 package sc.fiji;
 
 import org.scijava.command.Command;
+import org.scijava.plugin.Menu;
 import org.scijava.plugin.Plugin;
 
 @Plugin(type = Command.class, 
-		menuPath = "ThreeDViewer>AALaunch")
+		menu = {@Menu(label = "ThreeDViewer"),
+				@Menu(label = "Launch", weight = 3) })
 public class ThreeDViewerLauncher implements Command {
 	@Override
 	public void run() {	
