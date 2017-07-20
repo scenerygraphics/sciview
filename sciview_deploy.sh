a22ea1ae68b1d28bb7fc73f59edf3b65c663da70
Kyle Harrington <kharrington@uidaho.edu>
2017-07-20 10:40:32 +0200

    Updating build script

--- sciview_deploy.sh
+++ sciview_deploy.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $TRAVIS_BRANCH == 'master' ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]
+if [ "$TRAVIS_SECURE_ENV_VARS" = true \
+  -a "$TRAVIS_PULL_REQUEST" = false \
+  -a "$TRAVIS_BRANCH" = master \
+  -a "$TRAVIS_OS_NAME" == linux ]
 then
    mvn deploy --settings settings.xml
 # For update site deployment: temporarily disabled
@@ -12,5 +15,6 @@ then
 #   curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
 #   chmod a+x upload-site-simple.sh
 #   ./upload-site-simple.sh SciView SciView
+else
+    mvn install
 fi
-# update force pristine
