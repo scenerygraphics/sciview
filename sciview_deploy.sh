00fb87bdb46e67569531b6f8581bee98a001eda1
Kyle Harrington <kharrington@uidaho.edu>
2017-07-20 09:59:30 +0200

    Updating travis

---
+++ sciview_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'master' ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then
+   mvn deploy --settings settings.xml
+# For update site deployment: temporarily disabled
+#   curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
+#   unzip fiji-nojre.zip
+#   mv target/SciView* Fiji.app/jars/
+#   cp -r src/plugins/* Fiji.app/plugins/
+#   cd Fiji.app
+#   curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
+#   chmod a+x upload-site-simple.sh
+#   ./upload-site-simple.sh SciView SciView
+fi
+# update force pristine
