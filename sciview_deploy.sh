4f4956e7a998a0b2dc79999eb92b76d088137489
Kyle Harrington <kharrington@uidaho.edu>
2017-12-07 19:46:49 +0100

    sciview update site (wip)

--- sciview_deploy.sh
+++ sciview_deploy.sh
@@ -7,9 +7,12 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
 then
    mvn -Pdeploy-to-imagej deploy --settings settings.xml
    curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
-   unzip fiji-nojre.zip
+   unzip fiji-nojre.zip   
+   rm target/sciview*tests.jar
+   rm target/sciview*sources.jar
    mv target/sciview* Fiji.app/jars/
    # Handle dependencies
+   mvn -Dimagej.app.directory=Fiji.app/
    cd Fiji.app
    curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
    chmod a+x upload-site-simple.sh
