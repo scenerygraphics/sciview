497935d4465c65f2219bb8a0f184913bd51630d5
Curtis Rueden <ctrueden@wisc.edu>
2017-12-10 22:44:31 +0100

    sciview_deploy.sh: lean on populate_fiji.sh
    We can upload our freshly populated Fiji to the SciView update site.
    
    It is close to working. But there are JARs with duplicate classes,
    which the ImageJ updater detects as circular dependencies. :-(

--- sciview_deploy.sh
+++ sciview_deploy.sh
@@ -6,19 +6,16 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_OS_NAME" == linux ]
 then
   mvn -Pdeploy-to-imagej deploy --settings settings.xml
-  curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
-  unzip fiji-nojre.zip
-  rm target/sciview*tests.jar
-  rm target/sciview*sources.jar
-  mv target/sciview* Fiji.app/jars/
-  # Handle dependencies
-  echo "Fetching dependencies"
-  mvn -Dimagej.app.directory=Fiji.app/ -DdeleteOtherVersionsProperty=false
-  echo "Dependencies fetched"
+
+  echo
+  echo "====== Generating Fiji installation ======"
+  sh populate_fiji.sh
+
+  echo
+  echo "====== Uploading to SciView update site ======"
   cd Fiji.app
   curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
   chmod a+x upload-site-simple.sh
-  echo "Transmitting to update site"
   ./upload-site-simple.sh SciView Kharrington
 else
   mvn install
