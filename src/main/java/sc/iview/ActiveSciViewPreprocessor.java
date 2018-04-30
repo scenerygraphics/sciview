7774689d73f30b526548977c124a1287c60582aa
Curtis Rueden <ctrueden@wisc.edu>
2018-04-30 10:16:57 -0700

    ActiveSciViewPreprocessor: clean up javadoc

--- src/main/java/sc/iview/ActiveSciViewPreprocessor.java
+++ src/main/java/sc/iview/ActiveSciViewPreprocessor.java
@@ -36,17 +36,14 @@ import org.scijava.plugin.Plugin;
 
 /**
  * Fills single, unresolved module inputs with the active active
- * {@link SciView}. Hence, rather than a dialog prompting the user to - *
- * manually select an input, the active {@link SciView} is used automatically. -
- * *
+ * {@link SciView}. Hence, rather than a dialog prompting the user to manually
+ * select an input, the active {@link SciView} is used automatically.
  * <p>
- * - * In the case of more than one compatible parameter, the active - *
- * {@link SciView} is not used and instead the user must select. This behavior -
- * * is consistent with ImageJ v1.x. - *
+ * In the case of more than one compatible parameter, the active {@link SciView}
+ * is not used and instead the user must select.
  * </p>
  * 
  * @author Curtis Rueden
- * @author Mark Hiner hinerm at gmail.com
  */
 @Plugin(type = PreprocessorPlugin.class)
 public class ActiveSciViewPreprocessor extends SingleInputPreprocessor<SciView> {
