97ac0695e4118cbbdd2fffca6a27f8f5a192110b
Kyle I S Harrington <kephale@kyleharrington.com>
2018-06-19 09:55:16 +0100

    Update travis to enable triggerable update site upload

--- sciview_deploy.sh
+++ sciview_deploy.sh
@@ -3,9 +3,11 @@
 if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a "$TRAVIS_BRANCH" = master \
-  -a "$TRAVIS_OS_NAME" == linux ]
+  -a "$TRAVIS_OS_NAME" == linux \
+  -a "$TRAVIS_COMMIT_MESSAGE" == *"Trigger update site upload"* ]
 then
-  mvn -Pdeploy-to-imagej deploy --settings settings.xml
+  # We'll use the standard scijava script for maven deployment
+  #mvn -Pdeploy-to-imagej deploy --settings settings.xml
 
   echo
   echo "====== Generating Fiji installation ======"
@@ -17,6 +19,4 @@ then
   curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
   chmod a+x upload-site-simple.sh
   ./upload-site-simple.sh SciView Kharrington
-else
-  mvn install
 fi
