c475cb58c84bac5b7eb967b50d780bc2e1e222f1
Curtis Rueden <ctrueden@wisc.edu>
2018-07-18 12:25:50 -0500

    SciView: remove unused variables

--- src/main/java/sc/iview/SciView.java
+++ src/main/java/sc/iview/SciView.java
@@ -1131,9 +1131,7 @@ public class SciView extends SceneryBase {
 
         @SuppressWarnings("unchecked")
         Class<T> voxelType = ( Class<T> ) image.firstElement().getClass();
-        int bytesPerVoxel = image.firstElement().getBitsPerPixel() / 8;
         float minVal = Float.MIN_VALUE, maxVal = Float.MAX_VALUE;
-        NativeTypeEnum nType = null;
 
         if( voxelType == UnsignedByteType.class ) {
             minVal = 0;
@@ -1183,7 +1181,6 @@ public class SciView extends SceneryBase {
         @SuppressWarnings("unchecked")
         Class<T> voxelType = ( Class<T> ) image.firstElement().getClass();
         int bytesPerVoxel = image.firstElement().getBitsPerPixel() / 8;
-        float minVal = Float.MIN_VALUE, maxVal = Float.MAX_VALUE;
         NativeTypeEnum nType = null;
 
         if( voxelType == UnsignedByteType.class ) {
