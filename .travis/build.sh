7128bfbfaad094e7479f1d13a6a1996e1c24854d
Curtis Rueden <ctrueden@wisc.edu>
2018-10-17 12:03:04 -0500

    When doing a release, also upload to update site
    Travis CI builds and deploys releases when a release.properties
    file is present. After deploying the release, let's also run
    sciview_deploy.sh. In this way, the SciView update site will
    automatically be updated with every new SciView release!

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh $encrypted_eb7aa63bf7ac_key $encrypted_eb7aa63bf7ac_iv
+
+# For release builds, upload the result to the ImageJ update site.
+if [ "$TRAVIS_SECURE_ENV_VARS" = true \
+  -a "$TRAVIS_PULL_REQUEST" = false \
+  -a -f release.properties \
+  -a "$TRAVIS_OS_NAME" == linux ]
+then
+  source sciview_deploy.sh
+fi
