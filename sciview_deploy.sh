e112fa99a7029d9bba5f2cea8163ccb251f9fbb8
Kyle Harrington <kharrington@uidaho.edu>
2018-10-17 10:30:43 -0400

    Updating travis script to manage releases and ensure releases are uploaded to update site

--- sciview_deploy.sh
+++ sciview_deploy.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-echo "Deploy situation is:"
-echo "  TRAVIS_SECURE_ENV_VARS $TRAVIS_SECURE_ENV_VARS"
-echo "  TRAVIS_PULL_REQUEST $TRAVIS_PULL_REQUEST"
-echo "  TRAVIS_BRANCH $TRAVIS_BRANCH"
-echo "  TRAVIS_OS_NAME $TRAVIS_OS_NAME"
-echo "  TRAVIS_COMMIT_MESSAGE $TRAVIS_COMMIT_MESSAGE"
 
-required_message="Trigger update site upload"
+# We'll use the standard scijava script for maven deployment
+#mvn -Pdeploy-to-imagej deploy --settings settings.xml
 
-if [ "$TRAVIS_SECURE_ENV_VARS" = true \
-  -a "$TRAVIS_PULL_REQUEST" = false \
-  -a "$TRAVIS_BRANCH" = master \
-  -a "$TRAVIS_OS_NAME" == linux ] && \
-  [ -z "${TRAVIS_COMMIT_MESSAGE##*$required_message*}" ]
-then
-  # We'll use the standard scijava script for maven deployment
-  #mvn -Pdeploy-to-imagej deploy --settings settings.xml
+echo
+echo "====== Generating Fiji installation ======"
+sh populate_fiji.sh
 
-  echo
-  echo "====== Generating Fiji installation ======"
-  sh populate_fiji.sh
-
-  echo
-  echo "====== Uploading to SciView update site ======"
-  cd Fiji.app
-  curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
-  chmod a+x upload-site-simple.sh
-  ./upload-site-simple.sh SciView Kharrington
-fi
+echo
+echo "====== Uploading to SciView update site ======"
+cd Fiji.app
+curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
+chmod a+x upload-site-simple.sh
+./upload-site-simple.sh SciView Kharrington
