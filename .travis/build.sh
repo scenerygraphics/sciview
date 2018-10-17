2da6ba3c18122679c6612d24bb8cfc07a05c88d8
Curtis Rueden <ctrueden@wisc.edu>
2018-10-17 12:49:22 -0500

    Travis: check release.properties earlier
    The release process results in release.properties no longer being
    present. If we want to use that as an indicator for uploading to the
    udpate site, then we should check it first and remember for later.
    
    We also use sh instead of source now to trigger sciview_deploy.sh,
    just in case Travis has an issue with using source.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh $encrypted_eb7aa63bf7ac_key $encrypted_eb7aa63bf7ac_iv
 
-# For release builds, upload the result to the ImageJ update site.
+# Discern whether this is a release build.
 if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a -f release.properties ]
 then
-  source sciview_deploy.sh
+  echo '--> Release build detected. Will upload it to the update site.'
+  UPLOAD_TO_UPDATE_SITE=true
 fi
+
+# Perform the main build.
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
+sh travis-build.sh $encrypted_eb7aa63bf7ac_key $encrypted_eb7aa63bf7ac_iv
+
+# Upload release version to the ImageJ update site.
+test "$UPLOAD_TO_UPDATE_SITE" && sh sciview_deploy.sh
