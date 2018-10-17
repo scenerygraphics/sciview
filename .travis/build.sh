1673a11029a0d92872b51fe815a81c494fa9c757
Curtis Rueden <ctrueden@wisc.edu>
2018-10-17 12:08:52 -0500

    Travis: fix syntax of if statement
    We stop testing for Linux here, since right now Linux is the only
    platform anyway.

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,8 +5,7 @@ sh travis-build.sh $encrypted_eb7aa63bf7ac_key $encrypted_eb7aa63bf7ac_iv
 # For release builds, upload the result to the ImageJ update site.
 if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
-  -a -f release.properties \
-  -a "$TRAVIS_OS_NAME" == linux ]
+  -a -f release.properties ]
 then
   source sciview_deploy.sh
 fi
