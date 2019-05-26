d1722dec89b8028087a47ca677ee43866df43fc4
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-05-26 16:29:17 -0400

    Place spheres at joints on Line3D by default

--- src/main/java/sc/iview/shape/Line3D.java
+++ src/main/java/sc/iview/shape/Line3D.java
@@ -2,6 +2,7 @@ package sc.iview.shape;
 
 import graphics.scenery.Cylinder;
 import graphics.scenery.Node;
+import graphics.scenery.Sphere;
 import org.scijava.util.ColorRGB;
 import sc.iview.vector.ClearGLVector3;
 import sc.iview.vector.Vector3;
@@ -11,8 +12,10 @@ import java.util.List;
 
 public class Line3D extends Node {
     protected List<Node> edges;
+    protected List<Node> joints;
     protected double edgeWidth;
     protected ColorRGB defaultColor;
+    protected boolean sphereJoints = true;
 
     public Line3D() {
         edges = new ArrayList<>();
@@ -23,9 +26,16 @@ public class Line3D extends Node {
         this.edgeWidth = edgeWidth;
 
         edges = new ArrayList<>();
+        if( sphereJoints ) joints= new ArrayList<>();
         for( int k = 1; k < points.size(); k++) {
-            Node edge = Cylinder.betweenPoints(ClearGLVector3.convert(points.get(k - 1)), ClearGLVector3.convert(points.get(k)), (float) (edgeWidth * 2), 1f, 15);
+            Node edge = Cylinder.betweenPoints(ClearGLVector3.convert(points.get(k - 1)), ClearGLVector3.convert(points.get(k)), (float) edgeWidth, 1f, 15);
             addLine(edge);
+            if( sphereJoints && k < points.size()-1 ) {
+                Node joint = new Sphere((float) edgeWidth,15);
+                joint.setPosition(ClearGLVector3.convert(points.get(k)));
+                joints.add(joint);
+                addChild(joint);
+            }
         }
     }
 
