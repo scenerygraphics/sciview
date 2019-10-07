1b5aa379f2d75a26035820cb488428eefeef8603
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-10-07 10:05:05 -0400

    Test release of SciView Fiji

--- sciview_deploy_unstable.sh
+++ sciview_deploy_unstable.sh
@@ -14,3 +14,8 @@ cd Fiji.app
 curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
 chmod a+x upload-site-simple.sh
 ./upload-site-simple.sh SciView-Unstable Kharrington
+
+echo
+echo "===== Creating unstable release ====="
+cd ..
+zip SciView-Fiji.zip Fiji.app
