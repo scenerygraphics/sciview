9f0485ddeb913e788b733819b6f99f7992762330
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-11-05 14:33:17 -0500

    Add Utils method for converting between GLMatrix and AffineTransform3D

--- src/main/java/sc/iview/Utils.java
+++ src/main/java/sc/iview/Utils.java
@@ -1,10 +1,17 @@
 package sc.iview;
 
+import cleargl.GLMatrix;
 import cleargl.GLVector;
+import net.imglib2.realtransform.AffineTransform3D;
 import org.scijava.util.ColorRGB;
 import org.scijava.util.ColorRGBA;
 
 public class Utils {
+    /**
+     * Convert a SciJava color to a GLVector
+     * @param color
+     * @return GLVector version of this color (normalized to 0-1)
+     */
     static public GLVector convertToGLVector(ColorRGB color) {
         if( color instanceof ColorRGBA) {
             return new GLVector( color.getRed() / 255f, //
@@ -15,5 +22,19 @@ public class Utils {
         return new GLVector( color.getRed() / 255f, //
                              color.getGreen() / 255f, //
                              color.getBlue() / 255f );
+    }
+
+    static public AffineTransform3D convertGLMatrixToAffineTransform3D(GLMatrix tmat) {
+        AffineTransform3D tform = new AffineTransform3D();
+        double[] ds = new double[16];
+        int k=0;
+        for( int r=0; r<4; r++ ) {
+            for (int c = 0; c < 4; c++) {
+                ds[k] = tmat.get(r, c);
+                k++;
+            }
+        }
+        tform.set(ds);
+        return tform;
     }
 }
