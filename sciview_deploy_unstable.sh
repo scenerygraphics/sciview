379932325f09ccfea4316526ee4746f59d172446
Ulrik Guenther <hello@ulrik.is>
2020-04-19 19:30:38 +0200

    Update deployment scripts, remove Trainable_Segmentation JAR

--- sciview_deploy_unstable.sh
+++ sciview_deploy_unstable.sh
@@ -11,9 +11,9 @@ sh populate_fiji.sh
 echo
 echo "====== Uploading to SciView update site ======"
 cd Fiji.app
-curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
-chmod a+x upload-site-simple.sh
-./upload-site-simple.sh SciView-Unstable Kharrington
+#curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
+#chmod a+x upload-site-simple.sh
+../upload-site-simple.sh SciView-Unstable Kharrington
 
 echo
 echo "===== Creating unstable release ====="
