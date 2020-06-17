e113e54ef3ee42507996d06fda2786e5c05b981b
Vladimír Ulman <ulman@fi.muni.cz>
2020-06-17 13:39:00 +0200

    FIX: NodeTranslateControl was changing camera's Up,Right & Forward vectors

--- src/main/java/sc/iview/controls/behaviours/NodeTranslateControl.java
+++ src/main/java/sc/iview/controls/behaviours/NodeTranslateControl.java
@@ -1,10 +1,10 @@
 package sc.iview.controls.behaviours;
 
-import graphics.scenery.Camera;
+import sc.iview.SciView;
 import graphics.scenery.Node;
+import org.joml.Vector3f;
 import org.scijava.ui.behaviour.DragBehaviour;
 import org.scijava.ui.behaviour.ScrollBehaviour;
-import sc.iview.SciView;
 
 /**
  * Control behavior for moving a Node
@@ -36,10 +36,6 @@ public class NodeTranslateControl implements DragBehaviour, ScrollBehaviour {
         setDragSpeed(dragSpeed);
     }
 
-    public Camera getCamera() {
-        return sciView.getCamera();
-    }
-
     /**
      * This function is called upon mouse down and initializes the camera control
      * with the current window size.
@@ -60,10 +56,10 @@ public class NodeTranslateControl implements DragBehaviour, ScrollBehaviour {
         final Node targetedNode = sciView.getActiveNode();
         if (targetedNode == null || !targetedNode.getLock().tryLock()) return;
 
-        targetedNode
-                .getPosition()
-                .add( getCamera().getRight().mul(( x - lastX ) * dragXySpeed))
-                .add( getCamera().getUp().mul(   ( lastY - y ) * dragXySpeed));
+        sciView.getCamera().getRight().mul(( x - lastX ) * dragXySpeed, dragPosUpdater);
+        targetedNode.getPosition().add(dragPosUpdater);
+        sciView.getCamera().getUp().mul(   ( lastY - y ) * dragXySpeed, dragPosUpdater);
+        targetedNode.getPosition().add(dragPosUpdater);
         targetedNode.setNeedsUpdate(true);
         //nothing works...
         //we need to have "targetedNode.getMaximumBoundingBox().getBoundingSphere().getOrigin()" updated
@@ -77,6 +73,7 @@ public class NodeTranslateControl implements DragBehaviour, ScrollBehaviour {
         lastX = x;
         lastY = y;
     }
+    private final Vector3f dragPosUpdater = new Vector3f();
 
     @Override public void end( int x, int y ) {
         firstEntered = true;
@@ -87,11 +84,11 @@ public class NodeTranslateControl implements DragBehaviour, ScrollBehaviour {
         final Node targetedNode = sciView.getActiveNode();
         if (targetedNode == null || !targetedNode.getLock().tryLock()) return;
 
-        targetedNode
-                .getPosition()
-                .add( getCamera().getForward().mul(-1f * (float)wheelRotation * scrollSpeed) );
+        sciView.getCamera().getForward().mul(-1f * (float)wheelRotation * scrollSpeed, scrollPosUpdater);
+        targetedNode.getPosition().add(scrollPosUpdater);
         targetedNode.setNeedsUpdate(true); //to be consistent with how it is done in drag() above
 
         targetedNode.getLock().unlock();
     }
+    private final Vector3f scrollPosUpdater = new Vector3f();
 }
