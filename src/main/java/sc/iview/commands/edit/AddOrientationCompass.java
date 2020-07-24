badf94dff433d72668e04276f80d315f8142cb7a
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-07-24 06:57:25 -0400

    Use better-arcball branch of scenery, naive attempt at camera-relative compass

--- src/main/java/sc/iview/commands/edit/AddOrientationCompass.java
+++ src/main/java/sc/iview/commands/edit/AddOrientationCompass.java
@@ -30,15 +30,19 @@ package sc.iview.commands.edit;
 
 import graphics.scenery.Cylinder;
 import graphics.scenery.Node;
+import org.joml.Matrix4f;
 import org.joml.Quaternionf;
 import org.scijava.command.Command;
+import org.scijava.command.CommandService;
 import org.scijava.plugin.Menu;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-
 import sc.iview.SciView;
 
-import static sc.iview.commands.MenuWeights.*;
+import java.util.HashMap;
+
+import static sc.iview.commands.MenuWeights.EDIT;
+import static sc.iview.commands.MenuWeights.EDIT_ADD_COMPASS;
 
 /**
  * Command to orientation compass (R,G,B cylinders oriented along X,Y,Z axes, respectively) to the scene
@@ -65,7 +69,7 @@ public class AddOrientationCompass implements Command {
 
     @Override
     public void run() {
-        final Node compassRootNode = new Node("Scene orientation compass");
+        final Node root = new Node("Scene orientation compass");
 
         //NB: RGB colors ~ XYZ axes
         //x axis:
@@ -73,21 +77,51 @@ public class AddOrientationCompass implements Command {
         axisNode.setName("compass axis: X");
         axisNode.getMaterial().getDiffuse().set(1f,0f,0f);
         axisNode.setRotation( new Quaternionf().rotateXYZ(0,0,(float)(-0.5*Math.PI)) );
-        compassRootNode.addChild( axisNode );
+        root.addChild( axisNode );
 
         //y axis:
         axisNode = new Cylinder(AXESBARRADIUS,yAxisLength,4);
         axisNode.setName("compass axis: Y");
         axisNode.getMaterial().getDiffuse().set(0f,1f,0f);
-        compassRootNode.addChild( axisNode );
+        root.addChild( axisNode );
 
         //z axis:
         axisNode = new Cylinder(AXESBARRADIUS,zAxisLength,4);
         axisNode.setName("compass axis: Z");
         axisNode.getMaterial().getDiffuse().set(0f,0f,1f);
         axisNode.setRotation( new Quaternionf().rotateXYZ((float)(0.5*Math.PI),0,0) );
-        compassRootNode.addChild( axisNode );
+        root.addChild( axisNode );
 
-        sciView.addNode( compassRootNode );
+        sciView.addNode( root );
+
+        sciView.getCamera().addChild(root);
+        //root.setPosition( new Vector3f(-25, 0, -100));
+
+        root.getUpdate().add(() -> {
+            root.setWantsComposeModel(false);
+            root.getModel().identity();
+
+            root.getModel().translate(
+                    sciView.getCamera().getPosition().add(
+                            root.getPosition() ) );
+            root.getModel().mul( new Quaternionf().get(new Matrix4f()) );
+
+//                logger.info("Updating pose of $controller, ${node.model}")
+
+            root.setNeedsUpdate(false);
+            root.setNeedsUpdateWorld(false);
+            return null;
+        });
+
+    }
+
+    public static void main(String... args) throws Exception {
+        SciView sv = SciView.create();
+
+        CommandService command = sv.getScijavaContext().getService(CommandService.class);
+
+        HashMap<String, Object> argmap = new HashMap<>();
+
+        command.run(AddOrientationCompass.class, true, argmap);
     }
 }
