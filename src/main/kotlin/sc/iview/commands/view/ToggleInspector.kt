5ec505522c2f06dbf1e9d830ab37ada0235d361a
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-10-05 20:55:26 +0200

    Convert ToggleInspector to Kotlin

--- src/main/kotlin/sc/iview/commands/view/ToggleInspector.kt
+++ src/main/kotlin/sc/iview/commands/view/ToggleInspector.kt
@@ -26,32 +26,27 @@
  * POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
-package sc.iview.commands.view;
+package sc.iview.commands.view
 
-import org.scijava.command.Command;
-import org.scijava.plugin.Menu;
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
-
-import sc.iview.SciView;
-
-import static sc.iview.commands.MenuWeights.*;
+import org.scijava.command.Command
+import org.scijava.plugin.Menu
+import org.scijava.plugin.Parameter
+import org.scijava.plugin.Plugin
+import sc.iview.SciView
+import sc.iview.commands.MenuWeights.VIEW
+import sc.iview.commands.MenuWeights.VIEW_TOGGLE_INSPECTOR
 
 /**
- * Command that displays a {@link NodePropertyEditor} window.
+ * Command that displays a [NodePropertyEditor] window.
  *
  * @author Curtis Rueden
  */
-@Plugin(type = Command.class, initializer = "initValues", menuRoot = "SciView", //
-        menu = { @Menu(label = "View", weight = VIEW), //
-                 @Menu(label = "Toggle Inspector", weight = VIEW_TOGGLE_INSPECTOR) })
-public class ToggleInspector implements Command {
-
+@Plugin(type = Command::class, initializer = "initValues", menuRoot = "SciView", menu = [Menu(label = "View", weight = VIEW), Menu(label = "Toggle Inspector", weight = VIEW_TOGGLE_INSPECTOR)])
+class ToggleInspector : Command {
     @Parameter
-    private SciView sciView;
+    private lateinit var sciView: SciView
 
-    @Override
-    public void run() {
-        sciView.toggleInspectorWindow();
+    override fun run() {
+        sciView.toggleInspectorWindow()
     }
 }
