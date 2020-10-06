1c67e832c6259126772f24ba8a77f30e709b21b6
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-10-06 14:31:42 +0200

    Convert ToggleBoundingGrid to Kotlin

--- src/main/kotlin/sc/iview/commands/view/ToggleBoundingGrid.kt
+++ src/main/kotlin/sc/iview/commands/view/ToggleBoundingGrid.kt
@@ -26,57 +26,48 @@
  * POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
-package sc.iview.commands.view;
+package sc.iview.commands.view
 
-import graphics.scenery.BoundingGrid;
-import graphics.scenery.Mesh;
-import graphics.scenery.Node;
-import org.scijava.command.Command;
-import org.scijava.log.LogService;
-import org.scijava.plugin.Menu;
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
-import sc.iview.SciView;
-
-import static sc.iview.commands.MenuWeights.*;
+import graphics.scenery.BoundingGrid
+import graphics.scenery.Mesh
+import graphics.scenery.Node
+import org.scijava.command.Command
+import org.scijava.log.LogService
+import org.scijava.plugin.Menu
+import org.scijava.plugin.Parameter
+import org.scijava.plugin.Plugin
+import sc.iview.SciView
+import sc.iview.commands.MenuWeights.VIEW
+import sc.iview.commands.MenuWeights.VIEW_TOGGLE_BOUNDING_GRID
 
 /**
  * Command to toggle the bounding grid around a Node
  *
  * @author Kyle Harrington
- *
  */
-@Plugin(type = Command.class, menuRoot = "SciView", //
-menu = {@Menu(label = "View", weight = VIEW), //
-        @Menu(label = "Toggle Bounding Grid", weight = VIEW_TOGGLE_BOUNDING_GRID)})
-public class ToggleBoundingGrid implements Command {
+@Plugin(type = Command::class, menuRoot = "SciView", menu = [Menu(label = "View", weight = VIEW), Menu(label = "Toggle Bounding Grid", weight = VIEW_TOGGLE_BOUNDING_GRID)])
+class ToggleBoundingGrid : Command {
+    @Parameter
+    private lateinit var logService: LogService
 
     @Parameter
-    private LogService logService;
+    private lateinit var sciView: SciView
 
     @Parameter
-    private SciView sciView;
+    private lateinit var node: Node
 
-    @Parameter
-    private Node node;
-
-    @Override
-    public void run() {
-        if( node instanceof Mesh ) {
-
-            if( node.getMetadata().containsKey("BoundingGrid") ) {
-                BoundingGrid bg = (BoundingGrid) node.getMetadata().get("BoundingGrid");
-                bg.setNode( null );
-                node.getMetadata().remove("BoundingGrid");
-                bg.getScene().removeChild(bg);
+    override fun run() {
+        if (node is Mesh) {
+            if (node.metadata.containsKey("BoundingGrid")) {
+                val bg = node.metadata["BoundingGrid"] as BoundingGrid?
+                bg!!.node = null
+                node.metadata.remove("BoundingGrid")
+                bg.getScene()!!.removeChild(bg)
             } else {
-                BoundingGrid bg = new BoundingGrid();
-                bg.setNode(node);
-
-                node.getMetadata().put("BoundingGrid", bg);
+                val bg = BoundingGrid()
+                bg.node = node
+                node.metadata["BoundingGrid"] = bg
             }
         }
-
     }
-
 }
