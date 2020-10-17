eac4e03ab0df4094727c9443513a2efeb6355782
Ulrik Guenther <hello@ulrik.is>
2020-10-17 16:15:57 +0200

    Travis CI: Make wget download of Miniconda less noisy

--- travis-build.sh
+++ travis-build.sh
@@ -186,7 +186,7 @@ then
 	if [ ! -f "$condaSh" ]; then
 		echo
 		echo "== Installing conda =="
-		wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+		wget -nv https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 		rm -rf "$condaDir"
 		bash miniconda.sh -b -p "$condaDir"
 		checkSuccess $?
