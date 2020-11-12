4bed0df23ddf3ef298f92358cefa99048fecedf2
Ulrik Guenther <hello@ulrik.is>
2020-11-12 17:34:54 +0100

    Add ImageJMain to have a way to launch sciview in conjunction with ImageJ from IDE

---
+++ src/main/java/sc/iview/ImageJMain.kt
@@ -0,0 +1,60 @@
+/*-
+ * #%L
+ * Scenery-backed 3D visualization package for ImageJ.
+ * %%
+ * Copyright (C) 2016 - 2020 SciView developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package sc.iview
+
+import net.imagej.lut.LUTService
+import net.imagej.ops.OpService
+import net.imagej.units.UnitService
+import org.scijava.Context
+import org.scijava.console.ConsoleService
+import org.scijava.io.IOService
+import org.scijava.menu.MenuService
+import org.scijava.options.OptionsService
+import org.scijava.tool.ToolService
+import org.scijava.ui.UIService
+import org.scijava.ui.swing.SwingIconService
+
+/**
+ * Entry point for testing SciView functionality.
+ *
+ * @author Kyle Harrington
+ * @author Ulrik Guenther
+ */
+object ImageJMain {
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val context = Context()
+        val uiService = context.service(UIService::class.java)
+        val sciViewService = context.service(SciViewService::class.java)
+
+        uiService.showUI()
+        sciViewService.createSciView()
+    }
+}
