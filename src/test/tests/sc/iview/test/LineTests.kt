ae4bc7e3813bf6f6cb4aec219b206d9a2bf7adf1
Domino2357 <JustinBuerger@googlemail.com>
2021-04-20 08:59:01 +0200

    LineTests: created new testclass for the 3D line.

---
+++ src/test/tests/sc/iview/test/LineTests.kt
@@ -0,0 +1,15 @@
+package sc.iview.test
+
+import junit.framework.Assert.assertNull
+import org.junit.Test
+import sc.iview.node.Line3D
+
+class LineTests {
+
+    //tests if a bounding box becomes null when Line does not implement HasGeometry
+    @Test
+    fun testBoundingBox() {
+        val line = Line3D()
+        assertNull(line.generateBoundingBox())
+    }
+}
