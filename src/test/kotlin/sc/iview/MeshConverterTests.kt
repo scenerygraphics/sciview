b880f7434fa34d0aefd13e639959ed685b35fcdb
Domino2357 <JustinBuerger@googlemail.com>
2021-04-24 19:04:00 +0200

    Added MeshConverterTests.kt

---
+++ src/test/kotlin/sc/iview/MeshConverterTests.kt
@@ -0,0 +1,13 @@
+package sc.iview.test
+
+import org.junit.Test
+import java.nio.FloatBuffer
+
+class MeshConverterTests {
+
+    @Test
+    fun testFlipVectorBuffer() {
+        val buffer  = floatArrayOf(1f, 3f, 4f, 5f, 6f, 7f, 8f)
+
+    }
+}
