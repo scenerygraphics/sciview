4b8d3abb98d5e3b05f7713f4dfcbce5247708588
unknown <justinBuerger@googlemail.com>
2021-04-27 16:08:04 +0200

    MeshConverterTests: finished test

--- src/test/kotlin/sc/iview/MeshConverterTests.kt
+++ src/test/kotlin/sc/iview/MeshConverterTests.kt
@@ -1,13 +1,20 @@
 package sc.iview.test
 
+import graphics.scenery.Mesh
+import graphics.scenery.Sphere
+import net.imagej.mesh.naive.NaiveDoubleMesh
 import org.junit.Test
-import java.nio.FloatBuffer
+import sc.iview.process.MeshConverter
+import kotlin.test.assertEquals
 
 class MeshConverterTests {
 
     @Test
-    fun testFlipVectorBuffer() {
-        val buffer  = floatArrayOf(1f, 3f, 4f, 5f, 6f, 7f, 8f)
+    fun testToScenery() {
+        val mesh = Mesh("TestMe")
+        val convertedMesh = MeshConverter.toImageJ(mesh)
+        val reConvertedMesh = MeshConverter.toScenery(convertedMesh)
 
+        assertEquals(mesh.children, reConvertedMesh.children)
     }
 }
