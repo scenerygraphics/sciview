54777daf2d1c4678e13f533aa1c0bd523786b1cd
unknown <justinBuerger@googlemail.com>
2021-04-28 14:59:13 +0200

    ControlPointTests: added new tests

--- src/test/kotlin/sc/iview/ControlPointTests.kt
+++ src/test/kotlin/sc/iview/ControlPointTests.kt
@@ -1,6 +1,7 @@
 package sc.iview
 
 import graphics.scenery.Node
+import org.joml.Vector3f
 import org.junit.Test
 import sc.iview.process.ControlPoints
 import kotlin.test.assertTrue
@@ -13,5 +14,22 @@ class ControlPointTests {
         val node = Node("testMe")
         controlPoints.addPoint(node)
         assertTrue(controlPoints.getNodes().contains(node))
+    }
+
+    @Test
+    fun testSetPoints() {
+        val controlPoints = ControlPoints()
+        val nodeList = arrayOf(Vector3f(1f, 2f, 3f), Vector3f(4f, 5f, 6f))
+        controlPoints.setPoints(nodeList)
+        assertTrue { controlPoints.getNodes()[0].position == Vector3f(1f, 2f, 3f) }
+    }
+
+    @Test
+    fun testInitializeSciView() {
+        val controlPoints = ControlPoints()
+        val sv = SciView.create()
+        val numberOfNodes = sv.allSceneNodes.size
+        controlPoints.initializeSciView(sv, 0f)
+        assertTrue  { sv.allSceneNodes.size == numberOfNodes+1 }
     }
 }
