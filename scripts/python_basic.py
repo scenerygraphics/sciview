0343d868be94a35803ab5a7cac3cc89d05d85019
Kyle Harrington <mdc@kyleharrington.com>
2021-05-03 08:49:32 +0200

    Update for comparing maven and gradle sciviews from python

--- scripts/python_basic.py
+++ scripts/python_basic.py
@@ -15,15 +15,25 @@ config.add_repositories(
 config.add_repositories({'jitpack': 'https://jitpack.io'})
 
 # Import/load dependencies
-dependencies = [
-    'net.imagej:imagej:2.1.0', 'graphics.scenery:scenery:4a0c1f7',
-    'sc.iview:sciview:ad0a6e5'
+# This is a maven based version, transitive versions behave properly
+sciview_ageratum = [
+    'graphics.scenery:scenery:5de0b1e', 'sc.iview:sciview:92add67'
 ]
+
+# There seem to be transitive dep issues with the gradle versions
+sciview_gradle = [
+    'graphics.scenery:scenery:4a0c1f7', 'sc.iview:sciview:0e36b9b'
+]
+
+dependencies = [
+    'net.imagej:imagej:2.1.0',
+] + sciview_ageratum
 
 for dep in dependencies:
     config.add_endpoints(dep)
 
 # Setup classes
+HashMap = jimport('java.util.HashMap')
 SciView = jimport('sc.iview.SciView')
 
 # Launch sciview
