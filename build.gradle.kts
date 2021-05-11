645d11157890b8c217cca4b87a81617ba9443345
Giuseppe Barbieri <elect86@gmail.com>
2021-05-11 09:57:26 +0200

    Cleaned some comments, renamed scenery version variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 import org.gradle.kotlin.dsl.implementation
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import sciview.api
 import sciview.implementation
 import sciview.joglNatives
-import sciview.lwjglNatives
 import java.net.URL
 
 plugins {
@@ -28,26 +26,19 @@ repositories {
 
 dependencies {
 
-    //    implementation(platform("sciJava:platform:30.0.0+6"))
-
     // Graphics dependencies
 
     annotationProcessor(sciJava.common)
     kapt(sciJava.common)
 
-    val scenery = "96e8a96"
-    api("graphics.scenery:scenery:$scenery") // { version { strictly(scenery) } }
-    // check if build is triggered
+    val sceneryVersion = "96e8a96"
+    api("graphics.scenery:scenery:$sceneryVersion")
+    // check if build is triggered on https://jitpack.io/#scenerygraphics/sciview `build` tab
     // if not, uncomment this only to trigger it
 //    api("com.github.scenerygraphics:scenery:$scenery")
 
+    // This seams to be still necessary
     implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
-    //    listOf("", "-glfw", "-jemalloc", "-vulkan", "-opengl", "-openvr", "-xxhash", "-remotery").forEach {
-    //        if (it == "-vulkan")
-    //            implementation("org.lwjgl:lwjgl$it")
-    //        else
-    //            implementation("org.lwjgl:lwjgl$it", lwjglNatives)
-    //    }
 
     implementation(misc.cleargl)
     implementation(misc.coreMem)
@@ -57,7 +48,7 @@ dependencies {
 
     // SciJava dependencies
 
-    implementation(sciJava.common) // { version { strictly("2.83.3") } } CHECKME
+    implementation(sciJava.common)
     implementation(sciJava.uiBehaviour)
     implementation(sciJava.scriptEditor)
     implementation(sciJava.uiSwing)
@@ -94,24 +85,6 @@ dependencies {
     // Kotlin dependencies
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-
-    // Optional dependencies - for sc.iview.Main only! -->
-    //		<dependency>
-    //			<groupId>org.scijava</groupId>
-    //			<artifactId>scijava-plugins-commands</artifactId>
-    //			<scope>runtime</scope>
-    //			<optional>true</optional>
-    //		</dependency>
-    //		<dependency>
-    //			<groupId>net.imagej</groupId>
-    //			<artifactId>imagej-plugins-commands</artifactId>
-    //			<scope>runtime</scope>
-    //			<optional>true</optional>
-    //		</dependency>
-    //		<dependency>
-    //			<groupId>ch.qos.logback</groupId>
-    //			<artifactId>logback-classic</artifactId>
-    //		</dependency>
 
     // Test scope
 
@@ -150,7 +123,6 @@ tasks {
         }
         sourceCompatibility = project.properties["sourceCompatibility"]?.toString() ?: default
     }
-    // https://docs.gradle.org/current/userguide/java_testing.html#test_filtering
     test {
         finalizedBy(jacocoTestReport) // report is always generated after tests run
     }
@@ -178,7 +150,7 @@ tasks {
             xml.isEnabled = true
             html.apply {
                 isEnabled = false
-                //                destination = file("$buildDir/jacocoHtml")
+                //destination = file("$buildDir/jacocoHtml")
             }
         }
         dependsOn(test) // tests are required to run before generating the report
@@ -189,7 +161,7 @@ tasks {
         main = "sc.iview.commands.demo.basic.MeshDemo"
 
         // arguments to pass to the application
-        //    args 'appArg1'
+        //    args("appArg1")
     }
 }
 
