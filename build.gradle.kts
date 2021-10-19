e2bc8a6d081b3ae726f00a631b768f0c1dac033f
Kyle Harrington <mdc@kyleharrington.com>
2021-10-19 12:06:36 +0200

    Fix jackson versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,9 +70,9 @@ dependencies {
     //    }
     //implementation(jackson.bundles.all)
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:1.12.5")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:1.12.5")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:1.12.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.5")
     implementation("org.msgpack:jackson-dataformat-msgpack:0.8.20")
 
     implementation(misc.cleargl)
