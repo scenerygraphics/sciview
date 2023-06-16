370c8dab95cac62e7b8ee4f6ffd493a7b367b198
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2023-06-16 15:29:53 +0200

    SwingSwapchain/REPLPane: Do UI initialisation on EDT, initialise REPL asynchronously

--- src/main/java/sc/iview/ui/REPLPane.java
+++ src/main/java/sc/iview/ui/REPLPane.java
@@ -75,7 +75,7 @@ public class REPLPane implements UIComponent<JComponent> {
     final JScrollPane outputScroll = new JScrollPane(output);
     outputScroll.setPreferredSize(new Dimension(440, 400));
 
-    repl = new ScriptREPL(context, output.getOutputStream());
+    repl = new ScriptREPL(context, "Python (Jython)", output.getOutputStream());
     repl.initialize();
 
     final Writer writer = output.getOutputWriter();
@@ -89,7 +89,7 @@ public class REPLPane implements UIComponent<JComponent> {
     //prompt = new REPLEditor(repl, vars, output);
     prompt = new REPLEditor(repl, null, output);
     context.inject(prompt);
-    prompt.setREPLLanguage("Python");
+
     final JScrollPane promptScroll = new JScrollPane(prompt);
 
     final JPanel bottomPane = new JPanel();
--- src/main/kotlin/sc/iview/ui/SwingMainWindow.kt
+++ src/main/kotlin/sc/iview/ui/SwingMainWindow.kt
@@ -46,6 +46,7 @@ import java.awt.event.*
 import java.util.*
 import javax.script.ScriptException
 import javax.swing.*
+import kotlin.concurrent.thread
 import kotlin.math.roundToInt
 
 /**
@@ -58,228 +59,239 @@ class SwingMainWindow(val sciview: SciView) : MainWindow {
     private val logger by lazyLogger()
     private var previousSidebarPosition = 0
 
-    var sceneryJPanel: SceneryJPanel
+    lateinit var sceneryJPanel: SceneryJPanel
         private set
-    var mainSplitPane: JSplitPane
+    lateinit var mainSplitPane: JSplitPane
         protected set
-    var inspector: JSplitPane
+    lateinit var inspector: JSplitPane
         protected set
-    var interpreterPane: REPLPane
+    lateinit var interpreterPane: REPLPane
         protected set
-    var nodePropertyEditor: SwingNodePropertyEditor
+    lateinit var nodePropertyEditor: SwingNodePropertyEditor
         protected set
-    var frame: JFrame
+    lateinit var frame: JFrame
         protected set
     var sidebarOpen = false
         protected set
 
-    private var splashLabel: SplashLabel
+    private lateinit var splashLabel: SplashLabel
     private var defaultSidebarAction: (() -> Any)? = null
 
     init {
-        FlatLightLaf.setup()
-        try {
-            UIManager.setLookAndFeel(FlatLightLaf())
-        } catch (ex: Exception) {
-            System.err.println("Failed to initialize Flat Light LaF, falling back to Swing default.")
-        }
-
-        var x: Int
-        var y: Int
-        try {
-            val screenSize = Toolkit.getDefaultToolkit().screenSize
-            x = screenSize.width / 2 - sciview.windowWidth / 2
-            y = screenSize.height / 2 - sciview.windowHeight / 2
-        } catch (e: HeadlessException) {
-            x = 10
-            y = 10
-        }
-
-        frame = JFrame("sciview")
-        frame.layout = BorderLayout(0, 0)
-        frame.setSize(sciview.windowWidth, sciview.windowHeight)
-        frame.setLocation(x, y)
-
-        splashLabel = SplashLabel()
-        val glassPane = frame.glassPane as JPanel
-        glassPane.isVisible = true
-        glassPane.layout = BorderLayout()
-        glassPane.isOpaque = false
-        glassPane.add(splashLabel, BorderLayout.CENTER)
-        glassPane.requestFocusInWindow()
-        glassPane.revalidate()
-
-        frame.defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
-        nodePropertyEditor = SwingNodePropertyEditor(sciview)
-
-        sceneryJPanel = SceneryJPanel()
-        JPopupMenu.setDefaultLightWeightPopupEnabled(false)
-        val swingMenuBar = JMenuBar()
-        val menus = sciview.scijavaContext?.getService(MenuService::class.java) ?: throw IllegalStateException("MenuService not available")
-        SwingJMenuBarCreator().createMenus(menus.getMenu("SciView"), swingMenuBar)
-
-        val bar = ProgressPie()
-        bar.value = 0.0
-        bar.minimumSize = Dimension(30, 30)
-        bar.maximumSize = Dimension(30, 30)
-        bar.preferredSize = Dimension(30, 30)
-        val progressLabel = JLabel("<html><strong></strong></html>")
-        progressLabel.horizontalAlignment = SwingConstants.RIGHT
-        swingMenuBar.add(Box.createHorizontalGlue())
-        swingMenuBar.add(progressLabel)
-        swingMenuBar.add(bar)
-        frame.jMenuBar = swingMenuBar
-
-        sciview.taskManager.update = { current ->
-            if(current != null) {
-                progressLabel.text = "<html><strong>${current.source}:</strong> ${current.status} </html>"
-                bar.value = current.completion.toDouble()
-            } else {
-                progressLabel.text = ""
-                bar.value = 0.0
-            }
-
-            bar.repaint()
-        }
-        sceneryJPanel.isVisible = true
-        nodePropertyEditor.component // Initialize node property panel
-
-        val inspectorTree = nodePropertyEditor.tree
-        inspectorTree.toggleClickCount = 0 // This disables expanding menus on double click
-        val inspectorProperties = nodePropertyEditor.props
-
-        val container = JPanel(CardLayout())
-
-        var propsPane = JScrollPane(inspectorProperties)
-        var treePane = JScrollPane(inspectorTree)
-        propsPane.verticalScrollBar.unitIncrement = 16
-        treePane.verticalScrollBar.unitIncrement = 16
-        inspector = JSplitPane(JSplitPane.VERTICAL_SPLIT,  //
-                treePane,
-                propsPane)
-        inspector.dividerLocation = sciview.windowHeight / 3
-        inspector.isContinuousLayout = true
-        inspector.border = BorderFactory.createEmptyBorder()
-        inspector.dividerSize = 1
-        inspector.name = "Inspector"
-        container.add(inspector, "Inspector")
-
-        // We need to get the surface scale here before initialising scenery's renderer, as
-        // the information is needed already at initialisation time.
-        val dt = frame.graphicsConfiguration.defaultTransform
-        val surfaceScale = Vector2f(dt.scaleX.toFloat(), dt.scaleY.toFloat())
-        sciview.getScenerySettings().set("Renderer.SurfaceScale", surfaceScale)
-        interpreterPane = REPLPane(sciview.scijavaContext)
-        interpreterPane.component.border = BorderFactory.createEmptyBorder()
-        interpreterPane.component.name = "REPL"
-        container.add(interpreterPane.component, "REPL")
-        initializeInterpreter()
-        mainSplitPane = JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-                                   sceneryJPanel,
-                                   container
-        )
-        mainSplitPane.dividerLocation = frame.width
-        mainSplitPane.border = BorderFactory.createEmptyBorder()
-        mainSplitPane.dividerSize = 1
-        mainSplitPane.resizeWeight = 0.5
-        mainSplitPane.rightComponent = null
-
-        val toolbar = JToolBar()
-        toolbar.orientation = SwingConstants.VERTICAL
-
-        val inspectorIcon = Utils.getScaledImageIcon(SciView::class.java.getResource("toolbox.png"), 16, 16)
-        val inspectorButton = JToggleButton()
-        inspectorButton.toolTipText = "Inspector"
-
-        val inspectorAction = object: AbstractAction("Inspector", inspectorIcon) {
-            override fun actionPerformed(e: ActionEvent) {
-                container.toggleSidebarComponent(inspector, toolbar, inspectorButton, e)
-            }
-        }
-
-        defaultSidebarAction = { container.toggleSidebarComponent(inspector, toolbar, inspectorButton, null) }
-
-        inspectorButton.action = inspectorAction
-        inspectorButton.icon = inspectorIcon
-        inspectorButton.hideActionText = true
-
-        val replIcon = Utils.getScaledImageIcon(SciView::class.java.getResource("terminal.png"), 16, 16)
-        val replButton = JToggleButton()
-        replButton.toolTipText = "Script Interpreter"
-
-        val replAction = object: AbstractAction("REPL", replIcon) {
-            override fun actionPerformed(e: ActionEvent) {
-                container.toggleSidebarComponent(interpreterPane.component, toolbar, replButton, e)
-            }
-        }
-
-        replButton.action = replAction
-        replButton.icon = replIcon
-        replButton.hideActionText = true
-
-        toolbar.add(inspectorButton)
-        toolbar.add(replButton)
-
-        //frame.add(mainSplitPane, BorderLayout.CENTER);
-        frame.add(mainSplitPane, BorderLayout.CENTER)
-        frame.add(toolbar, BorderLayout.EAST)
-        frame.defaultCloseOperation = JFrame.DO_NOTHING_ON_CLOSE
-        frame.addWindowListener(object : WindowAdapter() {
-            override fun windowClosing(e: WindowEvent) {
-                logger.debug("Closing SciView window.")
-                close()
-                sciview.scijavaContext?.service(SciViewService::class.java)?.close(sciview)
-                sciview.isClosed = true
-            }
-        })
-
-        frame.isVisible = true
-        glassPane.repaint()
-
-        sciview.sceneryPanel[0] = sceneryJPanel
-        val renderer = Renderer.createRenderer(
-                sciview.hub,
-                sciview.applicationName,
-                sciview.currentScene,
-                sciview.windowWidth,
-                sciview.windowHeight,
-                sciview.sceneryPanel[0]
-        )
-
-        sciview.setRenderer(renderer)
-        sciview.hub.add(SceneryElement.Renderer, renderer)
-        sciview.reset()
-
-        SwingUtilities.invokeLater {
+        val initializer = Runnable {
+            FlatLightLaf.setup()
             try {
-                while (!sciview.getSceneryRenderer()!!.firstImageReady) {
-                    logger.debug("Waiting for renderer initialisation")
-                    Thread.sleep(300)
-                }
-                Thread.sleep(200)
-            } catch (e: InterruptedException) {
-                logger.error("Renderer construction interrupted.")
+                UIManager.setLookAndFeel(FlatLightLaf())
+            } catch (ex: Exception) {
+                System.err.println("Failed to initialize Flat Light LaF, falling back to Swing default.")
             }
-            nodePropertyEditor.rebuildTree()
-            logger.info("Done initializing SciView")
 
-            // subscribe to Node{Added, Removed, Changed} events, happens automatically
-//            eventService!!.subscribe(this)
+            var x: Int
+            var y: Int
+            try {
+                val screenSize = Toolkit.getDefaultToolkit().screenSize
+                x = screenSize.width / 2 - sciview.windowWidth / 2
+                y = screenSize.height / 2 - sciview.windowHeight / 2
+            } catch (e: HeadlessException) {
+                x = 10
+                y = 10
+            }
+
+            frame = JFrame("sciview")
+            frame.layout = BorderLayout(0, 0)
+            frame.setSize(sciview.windowWidth, sciview.windowHeight)
+            frame.setLocation(x, y)
+
+            splashLabel = SplashLabel()
+            val glassPane = frame.glassPane as JPanel
+            glassPane.isVisible = true
+            glassPane.layout = BorderLayout()
+            glassPane.isOpaque = false
+            glassPane.add(splashLabel, BorderLayout.CENTER)
+            glassPane.requestFocusInWindow()
+            glassPane.revalidate()
+
+            frame.defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+            nodePropertyEditor = SwingNodePropertyEditor(sciview)
+
+            sceneryJPanel = SceneryJPanel()
+            JPopupMenu.setDefaultLightWeightPopupEnabled(false)
+            val swingMenuBar = JMenuBar()
+            val menus = sciview.scijavaContext?.getService(MenuService::class.java)
+                    ?: throw IllegalStateException("MenuService not available")
+            SwingJMenuBarCreator().createMenus(menus.getMenu("SciView"), swingMenuBar)
+
+            val bar = ProgressPie()
+            bar.value = 0.0
+            bar.minimumSize = Dimension(30, 30)
+            bar.maximumSize = Dimension(30, 30)
+            bar.preferredSize = Dimension(30, 30)
+            val progressLabel = JLabel("<html><strong></strong></html>")
+            progressLabel.horizontalAlignment = SwingConstants.RIGHT
+            swingMenuBar.add(Box.createHorizontalGlue())
+            swingMenuBar.add(progressLabel)
+            swingMenuBar.add(bar)
+            frame.jMenuBar = swingMenuBar
+
+            sciview.taskManager.update = { current ->
+                if (current != null) {
+                    progressLabel.text = "<html><strong>${current.source}:</strong> ${current.status} </html>"
+                    bar.value = current.completion.toDouble()
+                } else {
+                    progressLabel.text = ""
+                    bar.value = 0.0
+                }
+
+                bar.repaint()
+            }
             sceneryJPanel.isVisible = true
+            nodePropertyEditor.component // Initialize node property panel
 
-            // install hook to keep inspector updated on external changes (scripting, etc)
-            sciview.currentScene.onNodePropertiesChanged["updateInspector"] = { node: Node ->
-                if (node === nodePropertyEditor.currentNode) {
-                    nodePropertyEditor.updateProperties(node)
+            val inspectorTree = nodePropertyEditor.tree
+            inspectorTree.toggleClickCount = 0 // This disables expanding menus on double click
+            val inspectorProperties = nodePropertyEditor.props
+
+            val container = JPanel(CardLayout())
+
+            var propsPane = JScrollPane(inspectorProperties)
+            var treePane = JScrollPane(inspectorTree)
+            propsPane.verticalScrollBar.unitIncrement = 16
+            treePane.verticalScrollBar.unitIncrement = 16
+            inspector = JSplitPane(JSplitPane.VERTICAL_SPLIT,  //
+                    treePane,
+                    propsPane)
+            inspector.dividerLocation = sciview.windowHeight / 3
+            inspector.isContinuousLayout = true
+            inspector.border = BorderFactory.createEmptyBorder()
+            inspector.dividerSize = 4
+            inspector.name = "Inspector"
+            container.add(inspector, "Inspector")
+
+            // We need to get the surface scale here before initialising scenery's renderer, as
+            // the information is needed already at initialisation time.
+            val dt = frame.graphicsConfiguration.defaultTransform
+            val surfaceScale = Vector2f(dt.scaleX.toFloat(), dt.scaleY.toFloat())
+            sciview.getScenerySettings().set("Renderer.SurfaceScale", surfaceScale)
+            interpreterPane = REPLPane(sciview.scijavaContext)
+            interpreterPane.component.border = BorderFactory.createEmptyBorder()
+            interpreterPane.component.name = "REPL"
+            container.add(interpreterPane.component, "REPL")
+            thread {
+                initializeInterpreter()
+            }
+            mainSplitPane = JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                    sceneryJPanel,
+                    container
+            )
+            mainSplitPane.dividerLocation = frame.width
+            mainSplitPane.border = BorderFactory.createEmptyBorder()
+            mainSplitPane.dividerSize = 4
+            mainSplitPane.resizeWeight = 1.0
+            mainSplitPane.rightComponent = null
+
+            val toolbar = JToolBar()
+            toolbar.orientation = SwingConstants.VERTICAL
+
+            val inspectorIcon = Utils.getScaledImageIcon(SciView::class.java.getResource("toolbox.png"), 16, 16)
+            val inspectorButton = JToggleButton()
+            inspectorButton.toolTipText = "Inspector"
+
+            val inspectorAction = object : AbstractAction("Inspector", inspectorIcon) {
+                override fun actionPerformed(e: ActionEvent) {
+                    container.toggleSidebarComponent(inspector, toolbar, inspectorButton, e)
                 }
             }
 
-            // Enable push rendering by default
-            renderer.pushMode = true
-            sciview.camera!!.setPosition(1.65, 1)
-            glassPane.isVisible = false
+            defaultSidebarAction = { container.toggleSidebarComponent(inspector, toolbar, inspectorButton, null) }
 
-            sceneryJPanel.minimumSize = Dimension(256, 256)
+            inspectorButton.action = inspectorAction
+            inspectorButton.icon = inspectorIcon
+            inspectorButton.hideActionText = true
+
+            val replIcon = Utils.getScaledImageIcon(SciView::class.java.getResource("terminal.png"), 16, 16)
+            val replButton = JToggleButton()
+            replButton.toolTipText = "Script Interpreter"
+
+            val replAction = object : AbstractAction("REPL", replIcon) {
+                override fun actionPerformed(e: ActionEvent) {
+                    container.toggleSidebarComponent(interpreterPane.component, toolbar, replButton, e)
+                }
+            }
+
+            replButton.action = replAction
+            replButton.icon = replIcon
+            replButton.hideActionText = true
+
+            toolbar.add(inspectorButton)
+            toolbar.add(replButton)
+
+            //frame.add(mainSplitPane, BorderLayout.CENTER);
+            frame.add(mainSplitPane, BorderLayout.CENTER)
+            frame.add(toolbar, BorderLayout.EAST)
+            frame.defaultCloseOperation = JFrame.DO_NOTHING_ON_CLOSE
+            frame.addWindowListener(object : WindowAdapter() {
+                override fun windowClosing(e: WindowEvent) {
+                    logger.debug("Closing SciView window.")
+                    close()
+                    sciview.scijavaContext?.service(SciViewService::class.java)?.close(sciview)
+                    sciview.isClosed = true
+                }
+            })
+
+            frame.isVisible = true
+            glassPane.repaint()
+
+            sciview.sceneryPanel[0] = sceneryJPanel
+            val renderer = Renderer.createRenderer(
+                    sciview.hub,
+                    sciview.applicationName,
+                    sciview.currentScene,
+                    sciview.windowWidth,
+                    sciview.windowHeight,
+                    sciview.sceneryPanel[0]
+            )
+
+            sciview.setRenderer(renderer)
+            sciview.hub.add(SceneryElement.Renderer, renderer)
+            sciview.reset()
+
+            SwingUtilities.invokeLater {
+                try {
+                    while (!sciview.getSceneryRenderer()!!.firstImageReady) {
+                        logger.debug("Waiting for renderer initialisation")
+                        Thread.sleep(300)
+                    }
+                    Thread.sleep(200)
+                } catch (e: InterruptedException) {
+                    logger.error("Renderer construction interrupted.")
+                }
+                nodePropertyEditor.rebuildTree()
+                logger.info("Done initializing SciView")
+
+                // subscribe to Node{Added, Removed, Changed} events, happens automatically
+//            eventService!!.subscribe(this)
+                sceneryJPanel.isVisible = true
+
+                // install hook to keep inspector updated on external changes (scripting, etc)
+                sciview.currentScene.onNodePropertiesChanged["updateInspector"] = { node: Node ->
+                    if (node === nodePropertyEditor.currentNode) {
+                        nodePropertyEditor.updateProperties(node)
+                    }
+                }
+
+                // Enable push rendering by default
+                renderer.pushMode = true
+                sciview.camera!!.setPosition(1.65, 1)
+                glassPane.isVisible = false
+
+                sceneryJPanel.minimumSize = Dimension(256, 256)
+            }
+        }
+
+        if(SwingUtilities.isEventDispatchThread()) {
+            initializer.run()
+        } else {
+            SwingUtilities.invokeAndWait(initializer)
         }
     }
 
