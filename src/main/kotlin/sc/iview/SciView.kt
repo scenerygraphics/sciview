7d00eea7a5bdc09d910934895cd26a63e804b564
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2024-02-12 11:56:01 +0100

    SciView: setActive is only called when publish is true

--- src/main/kotlin/sc/iview/SciView.kt
+++ src/main/kotlin/sc/iview/SciView.kt
@@ -890,13 +890,14 @@ class SciView : SceneryBase, CalibratedRealInterval<CalibratedAxis> {
                 Utils.blockWhile({ this.find(n.name) == null }, 20)
                 //System.out.println("find(name) " + find(n.getName()) );
             }
-            // Set new node as active and centered
-            if (centerOnNewNodes) {
-                centerOnNode(n)
-                setActiveNode(n)
-            }
+
             if (activePublish) {
                 eventService.publish(NodeAddedEvent(n))
+                setActiveNode(n)
+                // Set new node as centered
+                if (centerOnNewNodes) {
+                    centerOnNode(n)
+                }
             }
         }
         return n
