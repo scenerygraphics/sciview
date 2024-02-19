8b36d6b88f9e6eff54a1b68caa4adb472fdc48ef
Kyle Harrington <czi@kyleharrington.com>
2024-02-19 10:09:01 -0500

    Switch to 0.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
         exclude("org.lwjgl")
     }
 
-    val sceneryVersion = "22e8047"
+    val sceneryVersion = "0.9.1"
     api("graphics.scenery:scenery:$sceneryVersion") {
         version { strictly(sceneryVersion) }
         exclude("org.biojava.thirdparty", "forester")
