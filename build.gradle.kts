e7fffbdde5f40dec7337a3aa93fec2f3cb5d741d
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2024-04-19 09:32:34 +0200

    fix: bump scenery version to 0.11.2 (again after merging with remote)

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
         exclude("org.lwjgl")
     }
 
-    val sceneryVersion = "0.11.1"
+    val sceneryVersion = "0.11.2"
     api("graphics.scenery:scenery:$sceneryVersion") {
         version { strictly(sceneryVersion) }
         exclude("org.biojava.thirdparty", "forester")
