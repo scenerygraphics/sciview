da9979a512e80ec4f9bacdeb746b8593f334bac0
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2024-08-23 13:54:48 +0200

    Add: test object for loading a volume and starting the eye tracking directly (using ugly hardcoded path)

---
+++ src/test/kotlin/sc/iview/StartEyeTrackingDirectly.kt
@@ -0,0 +1,38 @@
+package sc.iview
+
+import graphics.scenery.utils.lazyLogger
+import graphics.scenery.volumes.RAIVolume
+import graphics.scenery.volumes.TransferFunction
+import org.scijava.command.CommandService
+import org.scijava.ui.UIService
+import sc.iview.commands.demo.advanced.EyeTrackingDemo
+
+object StartEyeTrackingDirectly {
+
+    val logger by lazyLogger()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sv = SciView.create()
+        val context = sv.scijavaContext
+        val uiService = context?.service(UIService::class.java)
+        uiService?.showUI()
+
+        sv.open("C:/Software/datasets/MastodonTutorialDataset1/datasethdf5.xml")
+        val volumes = sv.findNodes { it.javaClass == RAIVolume::class.java }
+        volumes.first().let {
+            it as RAIVolume
+            it.minDisplayRange = 400f
+            it.maxDisplayRange = 1500f
+            val tf = TransferFunction()
+            tf.addControlPoint(0f, 0f)
+            tf.addControlPoint(1f, 1f)
+            it.transferFunction = tf
+        }
+
+        val command = sv.scijavaContext!!.getService(CommandService::class.java)
+        val argmap = HashMap<String, Any>()
+        command.run(EyeTrackingDemo::class.java, true, argmap)
+
+    }
+}
