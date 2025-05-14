b0673af835da440b76352a543a180e6c82a9400d
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2025-05-14 10:20:49 +0200

    ConfirmableClickBehaviour: add delayed execution parameter

--- src/main/kotlin/sc/iview/commands/demo/advanced/ConfirmableClickBehaviour.kt
+++ src/main/kotlin/sc/iview/commands/demo/advanced/ConfirmableClickBehaviour.kt
@@ -1,33 +1,49 @@
 package sc.iview.commands.demo.advanced
 
 import org.scijava.ui.behaviour.ClickBehaviour
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.concurrent.thread
 
 
 /**
  * [ClickBehaviour] that waits [timeout] for confirmation by re-executing the behaviour.
  * Executes [armedAction] on first invocation, and [confirmAction] on second invocation, if
- * it happens within [timeout].
- *
+ * it happens within [timeout].  * If [delayedExecution] is true,
+ * the [armedAction] will only be executed after the [timeout] ran out and no second click was registered.
  * @author Ulrik Guenther <hello@ulrik.is>
  */
-class ConfirmableClickBehaviour(val armedAction: (Long) -> Any, val confirmAction: (Long) -> Any, var timeout: Long = 3000): ClickBehaviour {
+class ConfirmableClickBehaviour(
+    val armedAction: (Long) -> Any,
+    val confirmAction: (Long) -> Any,
+    var timeout: Long = 3000,
+    val delayedExecution: Boolean = false
+) : ClickBehaviour {
     /** Whether the action is armed at the moment. Action becomes disarmed after [timeout]. */
     private var armed: Boolean = false
+
+    /** Whether the [confirmAction] was fired. Needed for [delayedExecution]. */
+    private var confirmed: AtomicBoolean = AtomicBoolean(false)
 
     /**
      * Action fired at position [x]/[y]. Parameters not used in VR actions.
      */
-    override fun click(x : Int, y : Int) {
-        if(!armed) {
+    override fun click(x: Int, y: Int) {
+        if (!armed) {
             armed = true
-            armedAction.invoke(timeout)
+            if (!delayedExecution) {
+                armedAction.invoke(timeout)
+            }
 
             thread {
                 Thread.sleep(timeout)
                 armed = false
+                // Only trigger the delayed armedAction if no confirmedAction was triggered in the meantime
+                if (delayedExecution && !confirmed.get()) {
+                    armedAction.invoke(timeout)
+                }
             }
         } else {
+            confirmed.set(true)
             confirmAction.invoke(timeout)
         }
     }
