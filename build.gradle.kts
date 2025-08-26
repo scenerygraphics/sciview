667837f73ade0943b69f678dfe094d7040f9fea3
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2025-08-26 11:02:32 +0200

    build: fix missing SL4J provider

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,7 +119,7 @@ dependencies {
     implementation(platform(kotlin("bom")))
     implementation(kotlin("stdlib-jdk8"))
     testImplementation(kotlin("test-junit"))
-    testImplementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 
     implementation("sc.fiji:bigdataviewer-core")
     implementation("sc.fiji:bigdataviewer-vistools")
