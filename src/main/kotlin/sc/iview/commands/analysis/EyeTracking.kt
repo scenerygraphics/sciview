d6048ceb272c5992c7cb7887b2d9df5a643a92a0
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2026-01-30 11:39:45 +0100

    Extract Vector3f.toDoubleArray, add toDoubleArray for Vector2f and Vector4f too

--- src/main/kotlin/sc/iview/commands/analysis/EyeTracking.kt
+++ src/main/kotlin/sc/iview/commands/analysis/EyeTracking.kt
@@ -387,10 +387,6 @@ class EyeTracking(
 
     }
 
-    private fun Vector3f.toDoubleArray(): DoubleArray {
-        return this.toFloatArray().map { it.toDouble() }.toDoubleArray()
-    }
-
     fun DoubleArray.toVector3f(): Vector3f {
         require(size == 3) { "DoubleArray must have exactly 3 elements" }
         return Vector3f(this[0].toFloat(), this[1].toFloat(), this[2].toFloat())
